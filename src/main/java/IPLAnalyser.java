e9f03df7375f83bc1b323718b629a112b83348f7
Swapnaneel Bhuiya <swapnaneel@Swapnaneels-MacBook-Air.local>
2020-11-01 14:36:26 +0530

    [Swapnaneel]REFACTORED: Follow DRY principles

--- src/main/java/IPLAnalyser.java
+++ src/main/java/IPLAnalyser.java
@@ -11,11 +11,11 @@ import java.util.Iterator;
 import java.util.List;
 
 public class IPLAnalyser {
-    List<IPLMostRuns> csvFileList;
-    List<IPLWickets> csvList;
+    List<IPLMostRuns> IPLFileList;
+    List<IPLWickets> IPLWicketList;
     public IPLAnalyser() {
-        this.csvFileList = new ArrayList<IPLMostRuns>();
-        this.csvList=new ArrayList<IPLWickets>();
+        this.IPLFileList = new ArrayList<IPLMostRuns>();
+        this.IPLWicketList=new ArrayList<IPLWickets>();
     }
     public int loadIPLdata(String csvFilePath) throws IPLAnalyserException {
         CsvToBean<IPLMostRuns> csvToBean;
@@ -23,9 +23,9 @@ public class IPLAnalyser {
             ICSVBuilder icsvBuilder= CSVBuilderFactory.createCSVBuilder();
             Iterator<IPLMostRuns> censusCSVIterator = icsvBuilder.getCSVFileIterator(reader,  IPLMostRuns.class);
             while (censusCSVIterator.hasNext()) {
-                this.csvFileList.add((censusCSVIterator.next()));
+                this.IPLFileList.add((censusCSVIterator.next()));
             }
-            return csvFileList.size();
+            return IPLFileList.size();
         }
         catch(IllegalStateException e) {
             throw new IPLAnalyserException(e.getMessage(),
@@ -41,9 +41,9 @@ public class IPLAnalyser {
             ICSVBuilder icsvBuilder= CSVBuilderFactory.createCSVBuilder();
             Iterator<IPLWickets> censusCSVIterator = icsvBuilder.getCSVFileIterator(reader,  IPLWickets.class);
             while (censusCSVIterator.hasNext()) {
-                this.csvList.add((censusCSVIterator.next()));
+                this.IPLWicketList.add((censusCSVIterator.next()));
             }
-            return csvList.size();
+            return IPLWicketList.size();
         }
         catch(IllegalStateException e) {
             throw new IPLAnalyserException(e.getMessage(),
@@ -55,55 +55,55 @@ public class IPLAnalyser {
     }
     public String topBattingAverage(String filePath) throws IPLAnalyserException {
         loadIPLdata(filePath);
-        if (csvFileList == null || csvFileList.size() == 0) {
+        if (IPLFileList == null || IPLFileList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLMostRuns> censusComparator= Comparator.comparing(census->census.avg);
         this.sort(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvFileList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLFileList);
         return sortedStateCensusJson;
     }
     public String topStrikeRate(String filePath) throws IPLAnalyserException {
         loadIPLdata(filePath);
-        if (csvFileList == null || csvFileList.size() == 0) {
+        if (IPLFileList == null || IPLFileList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLMostRuns> censusComparator= Comparator.comparing(census->census.sr);
         this.sort(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvFileList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLFileList);
         return sortedStateCensusJson;
     }
     public String maxFoursAndSixes(String filePath) throws IPLAnalyserException {
         loadIPLdata(filePath);
-        if (csvFileList == null || csvFileList.size() == 0) {
+        if (IPLFileList == null || IPLFileList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLMostRuns> censusComparator= Comparator.comparing(census->census.fours+census.sixes);
         this.sort(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvFileList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLFileList);
         return sortedStateCensusJson;
     }
 
     private void sort(Comparator<IPLMostRuns> censusComparator) {
-        for (int i = 0; i < csvFileList.size(); i++) {
-            for (int j = 0; j < csvFileList.size() - i - 1; j++) {
-                IPLMostRuns census1 = csvFileList.get(j);
-                IPLMostRuns census2 = csvFileList.get(j + 1);
+        for (int i = 0; i < IPLFileList.size(); i++) {
+            for (int j = 0; j < IPLFileList.size() - i - 1; j++) {
+                IPLMostRuns census1 = IPLFileList.get(j);
+                IPLMostRuns census2 = IPLFileList.get(j + 1);
                 if (censusComparator.compare(census1, census2) < 0) {
-                    csvFileList.set(j, census2);
-                    csvFileList.set(j + 1, census1);
+                    IPLFileList.set(j, census2);
+                    IPLFileList.set(j + 1, census1);
                 }
             }
         }
     }
     private void sortWicket(Comparator<IPLWickets> censusComparator) {
-        for (int i = 0; i < csvList.size(); i++) {
-            for (int j = 0; j < csvList.size() - i - 1; j++) {
-                IPLWickets census1 = csvList.get(j);
-                IPLWickets census2 = csvList.get(j + 1);
+        for (int i = 0; i < IPLWicketList.size(); i++) {
+            for (int j = 0; j < IPLWicketList.size() - i - 1; j++) {
+                IPLWickets census1 = IPLWicketList.get(j);
+                IPLWickets census2 = IPLWicketList.get(j + 1);
                 if (censusComparator.compare(census1, census2) > 0) {
-                    csvList.set(j, census2);
-                    csvList.set(j + 1, census1);
+                    IPLWicketList.set(j, census2);
+                    IPLWicketList.set(j + 1, census1);
                 }
             }
         }
@@ -111,148 +111,148 @@ public class IPLAnalyser {
 
     public String getPlayersWithTopSRandBoundary(String filePath) throws IPLAnalyserException {
         loadIPLdata(filePath);
-        if (csvFileList == null || csvFileList.size() == 0) {
+        if (IPLFileList == null || IPLFileList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLMostRuns> censusComparator= Comparator.comparing(IPLMostRuns::getSixes)
                                                             .thenComparing(census->census.fours)
                                                             .thenComparing(census->census.sr);
         this.sort(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvFileList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLFileList);
         return sortedStateCensusJson;
     }
 
     public String getPlayerWithTopAverageAndSR(String filePath) throws IPLAnalyserException {
         loadIPLdata(filePath);
-        if (csvFileList == null || csvFileList.size() == 0) {
+        if (IPLFileList == null || IPLFileList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLMostRuns> censusComparator= Comparator.comparing(IPLMostRuns::getAvg)
                 .thenComparing(census->census.sr);
         this.sort(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvFileList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLFileList);
         return sortedStateCensusJson;
     }
 
     public String getPlayerWithTopAverageAndRuns(String filePath) throws IPLAnalyserException {
         loadIPLdata(filePath);
-        if (csvFileList == null || csvFileList.size() == 0) {
+        if (IPLFileList == null || IPLFileList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLMostRuns> censusComparator= Comparator.comparing(IPLMostRuns::getAvg).thenComparing(census->census.runs);
         this.sort(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvFileList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLFileList);
         return sortedStateCensusJson;
     }
 
     public String getPlayerWithTopBowlingAverage(String filePath) throws IPLAnalyserException {
         loadIPLBowlingData(filePath);
-        if (csvList == null || csvList.size() == 0) {
+        if (IPLWicketList == null || IPLWicketList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLWickets> censusComparator= Comparator.comparing(census->census.avg);
         this.sortWicket(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLWicketList);
         return sortedStateCensusJson;
     }
 
     public String getPlayerWithTopSR(String filePath) throws IPLAnalyserException {
         loadIPLBowlingData(filePath);
-        if (csvList == null || csvList.size() == 0) {
+        if (IPLWicketList == null || IPLWicketList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLWickets> censusComparator= Comparator.comparing(census->census.strikeRate);
         this.sortWicket(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLWicketList);
         return sortedStateCensusJson;
     }
 
     public String getPlayerWithTopEconomy(String filePath) throws IPLAnalyserException {
         loadIPLBowlingData(filePath);
-        if (csvList == null || csvList.size() == 0) {
+        if (IPLWicketList == null || IPLWicketList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLWickets> censusComparator= Comparator.comparing(census->census.economy);
         this.sortWicket(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLWicketList);
         return sortedStateCensusJson;
     }
 
     public String getPlayerWithTopSRWithWickets(String filePath) throws IPLAnalyserException {
         loadIPLBowlingData(filePath);
-        if (csvList == null || csvList.size() == 0) {
+        if (IPLWicketList == null || IPLWicketList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLWickets> censusComparator= Comparator.comparing(IPLWickets::getStrikeRate)
                 .thenComparing(census->census.fiveWicket).thenComparing(census->census.fourWicket);
         this.sortWicket(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLWicketList);
         return sortedStateCensusJson;
     }
 
     public String getPlayerWithTopSRWAndAverage(String filePath) throws IPLAnalyserException {
         loadIPLBowlingData(filePath);
-        if (csvList == null || csvList.size() == 0) {
+        if (IPLWicketList == null || IPLWicketList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLWickets> censusComparator= Comparator.comparing(IPLWickets::getAvg)
                 .thenComparing(census->census.strikeRate);
         this.sortWicket(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLWicketList);
         return sortedStateCensusJson;
     }
 
     public String getPlayerWithMaxWickets(String filePath) throws IPLAnalyserException {
         loadIPLBowlingData(filePath);
-        if (csvList == null || csvList.size() == 0) {
+        if (IPLWicketList == null || IPLWicketList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLWickets> censusComparator= Comparator.comparing(census->census.wickets);
         this.sortWicket(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLWicketList);
         return sortedStateCensusJson;
     }
     public String getBatsmanWithMaxAverage(String filePath) throws IPLAnalyserException {
         loadIPLdata(filePath);
-        if (csvFileList == null || csvFileList.size() == 0) {
+        if (IPLFileList == null || IPLFileList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLMostRuns> censusComparator= Comparator.comparing(IPLMostRuns::getAvg);
         this.sort(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvFileList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLFileList);
         return sortedStateCensusJson;
     }
 
     public String getPlayerWithTopRuns(String filePath) throws IPLAnalyserException {
         loadIPLdata(filePath);
-        if (csvFileList == null || csvFileList.size() == 0) {
+        if (IPLFileList == null || IPLFileList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLMostRuns> censusComparator= Comparator.comparing(census->census.runs);
         this.sort(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvFileList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLFileList);
         return sortedStateCensusJson;
     }
 
     public String getPlayerWithTopHundreds(String filePath) throws IPLAnalyserException {
         loadIPLdata(filePath);
-        if (csvFileList == null || csvFileList.size() == 0) {
+        if (IPLFileList == null || IPLFileList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLMostRuns> censusComparator= Comparator.comparing(census->census.hundred);
         this.sort(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvFileList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLFileList);
         return sortedStateCensusJson;
     }
 
     public String getPlayerWithTopHundredsOrFifties(String filePath) throws IPLAnalyserException {
         loadIPLdata(filePath);
-        if (csvFileList == null || csvFileList.size() == 0) {
+        if (IPLFileList == null || IPLFileList.size() == 0) {
             throw new IPLAnalyserException("NO_CENSUS_DATA", IPLAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IPLMostRuns> censusComparator= Comparator.comparing(IPLMostRuns::getHundred).thenComparing(census->census.fifty);
         this.sort(censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(this.csvFileList);
+        String sortedStateCensusJson = new Gson().toJson(this.IPLFileList);
         return sortedStateCensusJson;
     }
 }
